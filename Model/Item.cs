4a062122c64a537d414e6fe22a472e77f3b145c3
Nguyễn Ngọc Cường <58522857+ChetDuoiKhiDanhRang@users.noreply.github.com>
2024-07-09 00:26:24 +0700

    Initialize

---
+++ Model/Item.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameTool.Model
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public string Location { get; set; }
+        public string FullName { get => Name+Extension;}
+        public string FullPath { get => Location + "\\" + FullName; }    
+        public Item Parent { get; set; }
+
+
+    }
+}
---
+++ ViewModel/ViewItem.cs
@@ -0,0 +1,22 @@
+﻿using RenameTool.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenameTool.ViewModel
+{
+    internal class ViewItem : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public Item _Item { get; set; }
+    }
+}
